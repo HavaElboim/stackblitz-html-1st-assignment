4d42ff0ee7a1c229d454358043a76b5c8a3c0bd4
HavaElboim <77002425+HavaElboim@users.noreply.github.com>
2023-05-30 20:04:29 +0300

    3 files modified

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-var counter = 1;
+var counter = 0;
 
-function addToCounter() {
-  counter++;
+counter_elt = document.querySelector('#counter-value');
+warning_elt = document.querySelector('#warning');
+
+function addToCounter(x) {
+  if (x === -1 && counter === 0) {
+    warning_elt.innerHTML = 'Cannot go below zero';
+  } else {
+    warning_elt.innerHTML = '';
+    counter = counter += x;
+
+    // show nothing if count does not have a value
+    counter_elt.innerHTML = counter;
+  }
 }
